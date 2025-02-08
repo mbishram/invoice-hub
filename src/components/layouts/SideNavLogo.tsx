e6ce0747bdb9a25cabc6094eec15dac98eb99614
Muh Bishram Y A A <m.bishram.yashir@gmail.com>
2025-02-08 19:35:56 +0800

    Move image to public folder

--- src/components/layouts/SideNavLogo.tsx
+++ src/components/layouts/SideNavLogo.tsx
@@ -4,9 +4,6 @@ import Image from "next/image";
 // ** MUI Imports
 import { Box, BoxProps } from "@mui/material";
 
-// ** Asset Imports
-import logo from "@/assets/images/logo.svg";
-
 // ** Config Imports
 import { passionOne } from "@/configs/font.configs";
 
@@ -23,7 +20,12 @@ export default function SideNavLogo(props: Omit<BoxProps, "children">) {
       fontSize="1.5rem"
       {...props}
     >
-      <Image src={logo} alt={`${APP_NAME}'s logo`} height={44} />
+      <Image
+        src="/images/logo.svg"
+        alt={`${APP_NAME}'s logo`}
+        height={44}
+        width={42}
+      />
       {APP_NAME}
     </Box>
   );
